d63034cfbe86f4ca2b56ad653758145a7c8b8a2e
ilterates <iltates@gmail.com>
2016-10-04 18:08:29 -0700

    trying to commit again

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,13 @@ function time() {
   minute = now.getMinutes();
   second = now.getSeconds();
   console.log(hour);
+  if ( hour < 10 ){
+    hour = "0" + hour;
+  } if ( minute < 10 ) {
+    minute = "0" + minute;
+  } if ( second < 10 ) {
+    second = "0" + second;
+  }
     $("#time").text( hour + ":" + minute + ":" + second );
   var t = setTimeout(time, 500);
 }
