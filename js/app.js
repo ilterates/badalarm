e872b77b2a9200c2e985b1ab0004892adecc3186
ilterates <iltates@gmail.com>
2016-10-25 18:57:16 -0700

    hopefully fixed the bug that caused the time to show minus occasionally

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 console.log("Connected");
 var hour, minute, second;
 var set = 0;
+var hourVal = 0;
+var minuteVal = 0;
+$("#alarm-hour").val(hourVal);
+$("#alarm-minute").val(minuteVal);
+
 var audio = new Audio('https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_boat.mp3');
 
 function time() {
@@ -19,44 +24,51 @@ function time() {
     $("#time").text( hour + ":" + minute + ":" + second );
   var t = setTimeout(time, 500);
 }
-// function setAlarm() {
-//   if ( set === 0 || undefined ) {
-//     set = 1;
-//   } else {
-//     set = 0;
-//     }
-// }
       // setting the alarm //
       // hour //
-var hourVal = $("#alarm-hour").val();
 $("#hour-up-arrow").click(function(){
   hourVal ++;
   $("#alarm-hour").val(hourVal);
-  if (hourVal >= 23) {
-    hourVal = -1;
+  if (hourVal >= 24) {
+    hourVal = 0;
+    $("#alarm-hour").val(hourVal);
   }
 });
 $("#hour-down-arrow").click(function(){
   hourVal --;
   $("#alarm-hour").val(hourVal);
   if (hourVal <= 0) {
-    hourVal = 24;
+    hourVal = 23;
+    $("#alarm-hour").val(hourVal);
   }
 });
     // minute //
-var minuteVal = $("#alarm-minute").val();
 $("#minute-up-arrow").click(function(){
   minuteVal ++;
   $("#alarm-minute").val(minuteVal);
-  if (minuteVal >= 59) {
-    minuteVal = -1;
+  if (minuteVal >= 60) {
+    minuteVal = 0;
+    $("#alarm-minute").val(minuteVal);
   }
+    // if a digit in hour input is below 10, a zero will be added in front
+    // if ( hourVal < 10 && hourVal > 0 ) {
+    //     hourVal = "0" + hourVal;
+    //   }
+
+    // if a digit in minute input is below 10, a zero will be added in front
+  // if (minuteVal < 10 && minuteVal > 0) {
+  //   minuteVal = "0" + minuteVal;
+  //   console.log("truye");
+  //   console.log(minuteVal);
+  // }
+  // console.log(minuteVal);
 });
 $("#minute-down-arrow").click(function(){
   minuteVal --;
   $("#alarm-minute").val(minuteVal);
-  if (minuteVal <= 0) {
-    minuteVal = 60;
+  if (minuteVal < 0) {
+    minuteVal = 59;
+    $("#alarm-minute").val(minuteVal);
   }
 });
 function alarm() {
