6421b82a4629568d7666ad9d762776420f6f3246
ilterates <iltates@gmail.com>
2016-11-16 13:08:44 -0800

    alarm now works using momentjs without am/pm

--- js/app.js
+++ js/app.js
@@ -1,35 +1,26 @@
 console.log("Connected!");
+console.log(moment.unix());
 var hour, minute, second;
-var set = 0;
+var set = true;
 var hourVal = 0;
 var minuteVal = 0;
-$("#alarm-hour").val(hourVal);
-$("#alarm-minute").val(minuteVal);
-
+var hour = moment().format('hh');
+var minute = moment().format("mm");
 var blaring = new Audio();
-var audioArr = new Array('https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_boat.mp3','https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_morty.mp3');
+var audioArr = new Array('https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_boat.mp3',
+                         'https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_morty.mp3');
 var random = _.sample(audioArr);
 blaring.src = random;
-function time() {
-  now = new Date();
-  hour = now.getHours();
-  minute = now.getMinutes();
-  second = now.getSeconds();
-  // this code adds zero in front of single numbers //
-  // if ( hour < 10 ){
-  //   hour = "0" + hour;
-  // } if ( minute < 10 ) {
-  //   minute = "0" + minute;
-  // } if ( second < 10 ) {
-  //   second = "0" + second;
-  // }
-    $("#time").text( hour + ":" + minute + ":" + second );
-  var t = setTimeout(time, 500);
+function badTime() {
+  var time = moment().format('hh:mm:ss a'); // November 15th 2016, 8:54:55 pm
+  $("#time").text( time );
+  var t = setTimeout(badTime, 500);
 }
       // setting the alarm //
       // hour //
 $("#hour-up-arrow").click(function(){
   hourVal ++;
+  // doubleDigit();
   $("#alarm-hour").val(hourVal);
   if (hourVal >= 24) {
     hourVal = 0;
@@ -40,6 +31,7 @@ $("#hour-up-arrow").click(function(){
 $("#hour-up-arrow, #hour-down-arrow, #alarm-hour").bind('mousewheel', function(e){
   if (e.originalEvent.wheelDelta / 120 > 0) {
     hourVal ++;
+    // doubleDigit();
     $("#alarm-hour").val(hourVal);
     if (hourVal >= 24) {
       hourVal = 0;
@@ -49,6 +41,7 @@ $("#hour-up-arrow, #hour-down-arrow, #alarm-hour").bind('mousewheel', function(e
 });
 $("#hour-down-arrow").click(function(){
   hourVal --;
+  // doubleDigit();
   $("#alarm-hour").val(hourVal);
   if (hourVal <= 0) {
     hourVal = 23;
@@ -58,6 +51,7 @@ $("#hour-down-arrow").click(function(){
 $("#hour-down-arrow, #hour-up-arrow, #alarm-hour").bind('mousewheel', function(e){
   if (e.originalEvent.wheelDelta / 120 < 0) {
     hourVal --;
+    // doubleDigit();
     $("#alarm-hour").val(hourVal);
     if (hourVal <= 0) {
       hourVal = 23;
@@ -68,27 +62,17 @@ $("#hour-down-arrow, #hour-up-arrow, #alarm-hour").bind('mousewheel', function(e
     // minute //
 $("#minute-up-arrow").click(function(){
   minuteVal ++;
+  // doubleDigit();
   $("#alarm-minute").val(minuteVal);
   if (minuteVal >= 60) {
     minuteVal = 0;
     $("#alarm-minute").val(minuteVal);
   }
-    // if a digit in hour input is below 10, a zero will be added in front
-    // if ( hourVal < 10 && hourVal > 0 ) {
-    //     hourVal = "0" + hourVal;
-    //   }
-
-    // if a digit in minute input is below 10, a zero will be added in front
-  // if (minuteVal < 10 && minuteVal > 0) {
-  //   minuteVal = "0" + minuteVal;
-  //   console.log("truye");
-  //   console.log(minuteVal);
-  // }
-  // console.log(minuteVal);
 });
 
 $("#minute-down-arrow").click(function(){
   minuteVal --;
+  // doubleDigit();
   $("#alarm-minute").val(minuteVal);
   if (minuteVal < 0) {
     minuteVal = 59;
@@ -99,6 +83,7 @@ $("#minute-down-arrow").click(function(){
 $("#minute-up-arrow, #minute-down-arrow, #alarm-minute").bind('mousewheel', function(e){
   if (e.originalEvent.wheelDelta / 120 > 0) {
     minuteVal ++;
+    // doubleDigit();
     $("#alarm-minute").val(minuteVal);
     if (minuteVal >= 60) {
       minuteVal = 0;
@@ -109,6 +94,7 @@ $("#minute-up-arrow, #minute-down-arrow, #alarm-minute").bind('mousewheel', func
 $("#minute-down-arrow, #minute-up-arrow, #alarm-minute" ).bind('mousewheel', function(e){
   if (e.originalEvent.wheelDelta / 0 < 120) {
     minuteVal --;
+    // doubleDigit();
     $("#alarm-minute").val(minuteVal);
     if (minuteVal < 0) {
       minuteVal = 59;
@@ -116,27 +102,28 @@ $("#minute-down-arrow, #minute-up-arrow, #alarm-minute" ).bind('mousewheel', fun
     }
   }
 });
-function alarm() {
+function badAlarm() {
   var alarmHour = $("#alarm-hour").val();
   var alarmMinute =$("#alarm-minute").val();
-  var hourString = hour.toString();
-  var minuteString = minute.toString();
-  // console.log(hour,hourString + 'Hour');
-  // console.log(minute,minuteString + 'minute');
-  if ( alarmHour === hourString && alarmMinute === minuteString && set === 1 ) {
+  var intHour = parseInt(hour);
+  var intMinute = parseInt(minute);
+  var alarmIntHour = parseInt(alarmHour);
+  var alarmIntMinute = parseInt(alarmMinute);
+  if ( alarmIntHour === intHour && alarmIntMinute === intMinute && set === true ) {
     playSound();
+    console.log("alarm");
   }
-  var check = setTimeout(alarm, 500);
+  var check = setTimeout(badAlarm, 500);
 }
   $("#set-button").on('click',function(){
     // e.stopImmediatePropagation();
-    if ( set === 0 ) {
-      set = 1;
-      alarm();
+    if ( set === false ) {
+      set = true;
+      badAlarm();
       $("#set-button").html("CANCEL");
       blaring.currentTime = 0;
     } else {
-      set = 0;
+      set = false;
       stopSound();
       $("#set-button").html("SET");
     }
@@ -149,3 +136,12 @@ function alarm() {
   function stopSound() {
     blaring.pause();
   }
+  // function doubleDigit() {
+  //   if ( $("#alarm-hour").val().length < 2 ) {
+  //     alarmHour = "0" + alarmHour;
+  //     $("#alarm-hour").val(alarmHour);
+  //   } if ( $("#alarm-minute").val().length < 2 ) {
+  //     alarmMinute = "0" + alarmMinute;
+  //     $("#alarm-minute").val(alarmMinute);
+  //   }
+  // }
