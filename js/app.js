bbd1bb132911a0c6a33178eeb1bdc622fabf5452
ilterates <iltates@gmail.com>
2017-01-07 00:13:07 -0800

    changed jsparrow file name in array

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var blaring = new Audio();
 var audioArr = new Array('https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_boat.mp3',
                          'https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_morty.mp3',
                          'https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_mkay.mp3',
-                         'https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_sparrow.mp3');
+                         'https://raw.githubusercontent.com/ilterates/badalarm/master/assets/_jsparrow.mp3');
 var random = _.sample(audioArr);
 blaring.src = random;
 function badTime() {
